f95fa7e79e35fe0a5f525e8b87120ce80e79f630
Ngeh-hermann <ngehhermann719@gmail.com>
2023-10-11 00:04:53 +0100

    Finished Create and Delete User, Error page handling

---
+++ src/components/Intro.jsx
@@ -0,0 +1,28 @@
+import { UserPlusIcon } from "@heroicons/react/24/solid"
+import { Form } from "react-router-dom"
+import homeIllustration from "../assets/illustration.jpg"
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div>
+                <h1>
+                    Take Control of <span className="accent">Your Money</span>
+                </h1>
+                <p>
+                    Personal budgeting is the secret to financial freedom. Start your journey today.
+                </p>
+                <Form method="post">
+                    <input type="text" name="userName" required placeholder="Enter your name" aria-label="Your Name" autoComplete="given-name" />
+                    <button type="submit" className="btn btn--dark">
+                        <span>Create Account</span>
+                        <UserPlusIcon width={20} />
+                    </button>
+                </Form>
+            </div>
+            <img src={homeIllustration} alt="person with miney" width={600} />
+        </div>
+    )
+}
+
+export default Intro
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import { useLoaderData } from "react-router-dom";
+import { fetchData } from "../helpers"
+import Intro from "../components/Intro";
+import { toast } from "react-toastify";
+
+//loader function
+export function dashboardLoader() {
+    const userName = fetchData("userName");
+    return {
+        userName
+    }
+}
+
+export async function dashboardAction({ request }) {
+    const data = await request.formData();
+    const formData = Object.fromEntries(data);
+    try {
+        localStorage.setItem("userName", JSON.stringify(formData.userName))
+        return toast.success(`Welcome ${formData.userName}`);
+    } catch (e) {
+        throw new Error("There was a problem when creating your account!");
+    }
+}
+
+const Dashboard = () => {
+    const { userName } = useLoaderData()
+    return (
+        <>
+            {userName ? (<p>{userName}</p>) : <Intro />}
+        </>
+    )
+}
+
+export default Dashboard
